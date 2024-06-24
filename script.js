ccbf2c2001b4da9a9903ddf7f98cb8b9bd8b917a
mariaclaving <142334986+mariaclaving@users.noreply.github.com>
2024-06-24 18:21:39 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+//Part 1
+//exercise: 
+// Output: two variables (first and last name)
+
+let firstName = "Gianna";
+let lastName = "Nannini";
+
+let fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+
+
+//exercise: upper case just one part of string
+// Output: "Dublin CODING School"
+
+
+let str = "Dublin Coding School";
+let newStr = str.replace("Coding", "CODING");
+console.log(newStr); 
+
+
+
+// exercise: reverse string
+// Output: "School Coding Dublin"
+
+let str = "Dublin Coding School";
+
+let reversedStr = str.split(' ').reverse().join(' ');
+console.log(reversedStr);
+
+
+
+
+
+
+
+
+
